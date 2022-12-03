c258c1d1bc8dda4c4722211e1bcc15de17137532
sharipoff-0-1 <tedcodes.01@gmail.com>
2022-12-03 21:18:59 +0100

    home page is ready

--- src/components/Why/index.jsx
+++ src/components/Why/index.jsx
@@ -4,7 +4,7 @@ import { Container, Content, Wrapper, Icon } from "./style";
 export const Why = () => {
   return (
     <Container>
-      <Content>
+      <Content wide={500}>
         <h1 className="title">Why Choose Us</h1>
         <h1 className="info">
           Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.
@@ -13,15 +13,35 @@ export const Why = () => {
       <Wrapper>
         <Content>
           <Icon.Email />
+          <div className="subTitle">Trusted By Thousands</div>
+          <div className="info">
+            With over 1 million+ homes for sale available on the website, Trulia
+            can match you with a house you will want to call home.
+          </div>
         </Content>
         <Content>
           <Icon.House />
+          <div className="subTitle">Financing Made Easy</div>
+          <div className="info">
+            With over 1 million+ homes for sale available on the website, Trulia
+            can match you with a house you will want to call home.
+          </div>
         </Content>
         <Content>
           <Icon.Calculator />
+          <div className="subTitle">See Neighborhoods</div>
+          <div className="info">
+            With over 1 million+ homes for sale available on the website, Trulia
+            can match you with a house you will want to call home.
+          </div>
         </Content>
         <Content>
           <Icon.Maps />
+          <div className="subTitle">Wide Renge Of Properties</div>
+          <div className="info">
+            With over 1 million+ homes for sale available on the website, Trulia
+            can match you with a house you will want to call home.
+          </div>
         </Content>
       </Wrapper>
     </Container>
--- src/components/Why/style.js
+++ src/components/Why/style.js
@@ -8,36 +8,49 @@ const Container = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
   position: relative;
   margin: auto;
+  margin-top: 96px;
   background-color: #f5f7fc;
+  padding: 48px 0;
 `;
 
 const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  text-align: center;
+  max-width: ${({ wide }) => (wide ? `${wide}px` : `${235}px`)};
+  width: 100%;
 `;
 const Wrapper = styled.div`
   max-width: 1440px;
-  padding: 96px 130px;
+  width: 100%;
+  padding: 0px 130px;
   margin-top: 40px;
 
   display: flex;
   justify-content: space-around;
   align-items: center;
-  height: 434px;
 `;
 
 const Icon = styled.div``;
 
 Icon.Email = styled(cardMassage)`
+  margin-bottom: 24px;
   & path {
     fill: #0061df;
   }
 `;
-Icon.Maps = styled(cardMaps)``;
-Icon.House = styled(cardHouse)``;
-Icon.Calculator = styled(calculator)``;
+Icon.Maps = styled(cardMaps)`
+  margin-bottom: 24px;
+`;
+Icon.House = styled(cardHouse)`
+  margin-bottom: 24px;
+`;
+Icon.Calculator = styled(calculator)`
+  margin-bottom: 24px;
+`;
 
 export { Container, Content, Wrapper, Icon };
